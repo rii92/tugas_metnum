e44f0a7bcb8cf82e6ae34caa4bf87a319ff2f7f7
rii92 <riofebri16@gmail.com>
2022-07-02 15:21:47 +0700

    integral sudah bisa

--- TugasAkhirMetnum.c
+++ TugasAkhirMetnum.c
@@ -3,6 +3,8 @@
 
 float titik[100][100];
 float cvalue[100];
+float koef[100];
+float xvalue[100];
 
 void aproxFungsi();
 void aproxTurunan();
@@ -12,6 +14,15 @@ void menentukanValueC(int m);
 float HasilPolinomial(float x, int m);
 float hasilKaliKurung(int m, float x);
 void printPolinomial(int m);
+void refresh(int m);
+void isvaluepolinomial(int m);
+void bentukPower(int m);
+void printFungsi(int m);
+float hasilFungsi(int m, float x);
+float integrasiMidpoint(int m, float a, float b);
+float integrasiTrapesium(int m, float a, float b);
+float integrasiSimpson(int m, float a, float b);
+
 
 int main()
 {
@@ -71,10 +82,14 @@ void aproxFungsi(){ // Approximasi Fungsi
     menentukanValueC(m);
 
     //print fungsi polinomial
-    printf("fungsi yang didapat:\n");
+    printf("\nfungsi yang didapat:\n");
     printPolinomial(m);
 
+    //ingin tahu nilai fungsi pada titik yang diberikan?
+    isvaluepolinomial(m);
+    
     //hapus fungsi dan titik
+    refresh(m);
 
     return 0;
 }
@@ -87,7 +102,62 @@ void aproxTurunan(){ // Approximasi Turunan
 
 
 void aproxIntegral(){ // Approximasi Integral
-    return 0;
+    printf("Approximasi Integral\n");
+    printf("===================\n");
+    printf("\n");
+    printf("Pilih kondisi:\n");
+    printf("1. Memiliki fungsi yang bisa diintegrasikan\n");
+    printf("2. hanya memiliki beberapa titik untuk di bentuk fungsi polinomial\n");
+    char answer;
+    scanf("%s", &answer);
+    printf("\n");
+    int m;
+    if (answer == '2'){
+        printf("Masukkan jumlah titik yang diketahui : ");
+        scanf("%d", &m);
+        printf("\n");
+        menentukanTitik(m);
+        for (int i = 0; i < m; i++){ //testing
+            printf("titik x%d dan y%d = %.2f dan %.2f\n", i + 1, i + 1, titik[0][i], titik[1][i]);
+        }
+        //Cari C value
+        menentukanValueC(m);
+        
+        //bentuk power polinomial
+        bentukPower(m);
+
+        printf("\nfungsi yang didapat:\n");
+        printFungsi(m);
+    }
+    else if (answer == '1'){
+        printf("Masukkan jumlah derajat fungsi?\n");
+        scanf("%d", &m);
+        printf("\n");
+        m = m + 1;
+        for (int i = 0; i < m; i++)
+        {
+            printf("Masukkan koefisien x^%d : ", i);
+            scanf("%f", &koef[i]);
+        }
+    } else {
+        printf("Pilihan tidak tersedia\n");
+        return 0;
+    }
+
+    printf("\nMetode integrasi:\n");
+    printf("masukkan batas bawah : ");
+    float a;
+    scanf("%f", &a);
+    printf("masukkan batas atas : ");
+    float b;
+    scanf("%f", &b);
+    printf("\n");
+    printf("Hasil integrasi berdasarkan metode:\n");
+    printf("1. Midpoint = %.2f \n", integrasiMidpoint(m, a, b));
+    printf("2. Trapesium = %.2f \n", integrasiTrapesium(m, a, b));
+    printf("3. Simpson = %.2f \n", integrasiSimpson(m, a, b));
+    
+    refresh(m);
 }
 
 void menentukanTitik(int m){
@@ -98,7 +168,6 @@ void menentukanTitik(int m){
         scanf("%f", &titik[0][i]);
         scanf("%f", &titik[1][i]);
     }
-    return 0;
 }
 
 void menentukanValueC(int m){
@@ -112,17 +181,15 @@ void menentukanValueC(int m){
             cvalue[i] = (titik[1][i] - HasilPolinomial(titik[0][i], i))/hasilKaliKurung(i, titik[0][i]);
         }
     }
-    return 0;
 }
 
 float HasilPolinomial(float x, int m){
     float hasil = 0;
-    float hasilKurung = 0;
     float variabelX = x;
     for (int i = 0; i < m; i++)
     {
         if(i == 0){
-            hasil =+ titik[1][i];
+            hasil =+ cvalue[i];
         }
         else{
             hasil =+ hasil + cvalue[i]*hasilKaliKurung(i, variabelX);
@@ -150,7 +217,7 @@ void printPolinomial(int m){
             if(cvalue[i]>=0){
                 printf("+%.2f",cvalue[i]);
             }else{
-                printf("%.2f",cvalue[i]*(-1));
+                printf("%.2f",cvalue[i]);
             }
             
             for (int j = 0; j < i; j++)
@@ -163,4 +230,120 @@ void printPolinomial(int m){
             }
         }
     }
+    printf("\n");
+}
+
+void refresh(int m){
+    for (int i = 0; i < m; i++)
+    {
+        titik[0][i] = 0;
+        titik[1][i] = 0;
+        cvalue[i] = 0;
+        koef[i] = 0;
+        xvalue[i] = 0;
+    }   
+}
+
+void isvaluepolinomial(int m){
+    char c = 'y';
+    while (1)
+    {
+        printf("Ingin tahu nilai fungsi pada titik yang diberikan? (y/n) : ");
+        scanf("%s", &c);
+        printf("\n");
+        if (c == 'y')
+        {
+            printf("Masukkan titik x : ");
+            float x;
+            scanf("%f", &x);
+            printf("\n");
+            printf("Nilai fungsi pada titik x%.2f adalah %.2f\n", x, HasilPolinomial(x, m));
+        }
+        else if (c == 'n')
+        {
+            break;
+        }
+        else
+        {
+            printf("Input tidak valid\n");
+        }
+    }
+}
+
+void bentukPower(int m){
+    for (int i = 0; i < m; i++){
+        koef[i] = cvalue[i];
+        xvalue[i] = titik[0][i];
+    }
+
+    for (int j = 0; j < m; j++){
+        for (int i = 0; i < m; i++){
+            if(i == 0){
+                koef[m-i-1] = koef[m-i-1];
+            } else{
+                koef[m-i-1] = koef[m-i-1] + (0-xvalue[m-i-1])*koef[m-i];
+            }
+        }
+ 
+        for (int z = 0; z < m; z++){
+            if(m-z-1 == 0){
+                xvalue[m-z-1] = 0;
+            } else{
+                xvalue[m-z-1] = xvalue[m-z-2];
+            }
+        }
+    }
+}
+
+void printFungsi(int m){
+    for (int i = 0; i < m; i++)
+    {
+        if(i==0){
+            printf("%.2f",koef[i]);
+        } else{
+            if(koef[i]>=0){
+                printf("+%.2f",koef[i]);
+            } else{
+                printf("%.2f",koef[i]);
+            }
+                printf("x^%d",i);
+        }
+    }
+}
+
+float hasilFungsi(int m, float x){
+    float hasil = 0;
+    for (int i = 0; i < m; i++)
+    {
+        hasil += koef[i]*pow(x,i);
+    }
+    return hasil;
+}
+
+float integrasiMidpoint(int m, float a, float b){
+    int jumlahtitik = m;
+    float range = (b-a)/2;
+    float mid = a + range;
+    float hasil = 2*range*hasilFungsi(jumlahtitik, mid);
+    printf("%.2f\n", hasilFungsi(jumlahtitik, mid));
+    return hasil;
+}
+
+float integrasiTrapesium(int m, float a, float b){
+    int jumlahtitik = m;
+    float batasBawah = a;
+    float batasAtas = b;
+    float range = b - a;
+    float hasil = range*0.5*(hasilFungsi(jumlahtitik, batasBawah) + hasilFungsi(jumlahtitik, batasAtas));
+    return hasil;
+}
+
+float integrasiSimpson(int m, float a, float b){
+    int jumlahtitik = m;
+    float batasBawah = a;
+    float batasAtas = b;
+    float range = (batasAtas-batasBawah)/2;
+    float mid = batasBawah - range;
+    float hasil = range*(hasilFungsi(jumlahtitik, batasBawah) + 4*hasilFungsi(jumlahtitik, mid) + hasilFungsi(jumlahtitik, batasAtas))/3;
+    return hasil;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,3 +1,2 @@
-i < m; i++){ //testing
-        printf("titik x%d dan y%d = %.2f dan %.2f\n", i + 1, i + 1, titik[0][i], titik[1][i]);
-    }
+
+    float batasAtas = b;
